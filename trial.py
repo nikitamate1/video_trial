6f9433f9b9f5bb0131695c6fab7589e307f303d0
nikitamate1 <160838112+nikitamate1@users.noreply.github.com>
2024-02-22 14:18:02 +0530

    first commit

---
+++ trial.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("Simple YouTube Video Embedding")
+
+    # Embed a YouTube video
+    st.video("https://www.youtube.com/watch?v=sbfhxB2CiS8")
+
+if __name__ == "__main__":
+    main()
